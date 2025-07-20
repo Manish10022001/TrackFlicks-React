26b3052fea65d8be5cf1c818f989f47e269af9cf
Manish10022001 <manishshirsat2850@gmail.com>
2025-07-20 21:31:27 +0530

    updated to load movie list on search

--- src/App.jsx
+++ src/App.jsx
@@ -7,46 +7,44 @@ import Main from "./components/main/Main";
 
 import { Logo, Search, NumResults } from "./components/nav-bar/NavBar";
 
-//import { ListBox } from "./components/main/movielist/Box";
-//import { WatchedBox } from "./components/main/watchedMovies/WatchedBox";
 import Box from "./components/main/movielist/Box";
 import { MovieList } from "./components/main/movielist/MovieList";
 import { Loader } from "./components/main/movielist/Loader";
 import WatchedSummary from "./components/main/watchedMovies/WatchedSummary";
 import WatchedMovieList from "./components/main/watchedMovies/WatchedMovieList";
-
 import { tempWatchedData } from "./data/tempWatchedData";
+
 const KEY = "b0a4f46f";
-const query = "dkfdjfkdjfk"
+const tempQuery = "Interstellar"
 export default function App() {
-  //const [isOpen1, setIsOpen1] = useState(true);//first box
-  //const [isOpen2, setIsOpen2] = useState(true);//second box
-  //const [movies, setMovies] = useState(tempMovieData); //tempmovie data
-  // const [watched, setWatched] = useState(tempWatchedData);
 
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  //not a right way to fetch data or api
-  // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data.search));
+  const [query, setQuery] = useState("");
+  {/*
+  useEffect(()=>{
+    console.log("after initial render -> with dependency array")
+  },[])
 
-  //use useEffect to register event, event is the function here which contain sideeffect which we want to register, so this function will execute afteer render.
-  //second argument pass empty array to useeffect, this means this effect will only execute as component mount
-  // useEffect(function(){
-  //   fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-  //   .then((res) => res.json())
-  //   .then((data) => setMovies(data.Search));
-  // },[])
+  useEffect(()=>{
+    console.log("after every render -> without dependency array")
+  })
+
+  useEffect(()=>{
+    console.log("with query as dependency array")
+  },[query])
+
+  console.log("C");
+  */}
 
   //async function
   useEffect(function () {
     async function fetchMovies() {
       try {
         setIsLoading(true);
-
+        setError("")
         const res = await fetch(
           `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
         ); //interstellar
@@ -55,26 +53,32 @@ export default function App() {
 
         const data = await res.json();
         //erro message if movie not found
-        if(data.Response === "False") throw new Error("Movie not found")
+        if(data.Response === "False" || !data.Search) throw new Error("Movie not found")
         setMovies(data.Search);
 
         //setIsLoading(false); //for it to disappear
       } catch (err) {
+        //console.log(err.message);
         setError(err.message);
       } finally{ //finally is used so that Loading... disappears otherwise both error msg and loading appears on screen
         setIsLoading(false); //for it to disappear
       }
     }
+    if (query.length <3){
+      setMovies([]);
+      setError("");
+      return;
+    }
     fetchMovies();
-  }, []);
+  }, [query]);
 
   //after useEffect, want to add simple loading indicator, so show movies are loading, 1.create state 2.call it in useEffect, 3. call it in App with condition and create simple component
+  //after loader we handled errors using try catch.
+  //now synchronize queris with movie data - so create we lift tthe state up from search component and pass props
   return (
     <div>
-      {/* <NavBar movies={movies} /> */}
-      {/* pass children as prop in navbar jsx. and call components inside it in here <NavBar> ... </NavBar> */}
-      {/* and pass children instead of movies prop to it and also insted of components */}
-      <NavBar>
+
+      <NavBar query={query} setQuery={setQuery}>
         {/* <Logo />
         <Search />   this is also ok*/}
         <NumResults movies={movies} />
@@ -82,12 +86,6 @@ export default function App() {
       {/* <Main movies={movies}> longer needed after giving children same for NavBar*/}
 
       <Main>
-        {/* <ListBox movies={movies} > not needeed after giving below line*/}
-        {/* list box and watched box contains same logic so we can create a resuable componet with it , so we change listbox to box and  */}
-        {/* now we don't need open1 and open2 , we can just use open state */}
-        {/* <ListBox >
-          <MovieList movies={movies} />
-        </ListBox> */}
         <Box>
           {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
           {isLoading && <Loader />}
